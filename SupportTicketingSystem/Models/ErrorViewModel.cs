9fd02d5bbca1d54d0c9fbf119d44515d6a0552b4
MariellaNik <mariela.nik02@gmail.com>
2025-02-09 16:59:03 +0200

    Remove all files of v1

--- SupportTicketingSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SupportTicketingSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
